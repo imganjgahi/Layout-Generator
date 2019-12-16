433a6686ceef11347f8d826c8c9f50e7307130c1
Mehran <imganjgahi@gmail.com>
2019-12-16 23:29:06 +0330

    dorpDown added in navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -10,6 +10,13 @@ export const Navbar = (props: IProps) => {
     return (
         <div className="navbar">
             <h1 className="title">Layout Generator</h1>
+            <select className="dropDown" onChange={(e) => {
+                console.log(e.target.value)
+            }}>
+                <option value="XL"> XL </option>
+                <option value="L"> L </option>
+                <option value="SM"> SM </option>
+            </select>
         </div>
     )
 }
